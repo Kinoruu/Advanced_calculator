128a71e0fbd574f906c473060aa4f4abdb11e4c4
jakub siejak <jakubsiejak@gmail.com>
2020-10-18 02:21:11 +0100

    Forst commit just few buttons, labels and tekstboxes

--- Advanced_calculator/Form1.Designer.cs
+++ Advanced_calculator/Form1.Designer.cs
@@ -35,7 +35,6 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -102,15 +101,6 @@
             this.label3.TabIndex = 6;
             this.label3.Text = "label3";
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(585, 199);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "label4";
-            // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(47, 196);
@@ -167,11 +157,11 @@
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(507, 178);
+            this.button7.Location = new System.Drawing.Point(564, 196);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(56, 54);
+            this.button7.Size = new System.Drawing.Size(56, 20);
             this.button7.TabIndex = 14;
-            this.button7.Text = "button7";
+            this.button7.Text = "Wynik";
             this.button7.UseVisualStyleBackColor = true;
             // 
             // button8
@@ -206,7 +196,6 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -230,7 +219,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
